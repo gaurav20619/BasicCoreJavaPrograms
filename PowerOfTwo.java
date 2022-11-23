01f7b6850e22a54f826097042127767ff1cac6ff
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-11-23 12:30:47 +0530

    Add: Power of two program.

---
+++ PowerOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PowerOfTwo {
+    public static void main(String[] args) {
+		int N, powerOfTwo, i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Power Value you want: ");
+        N = sc.nextInt();
+        sc.close();
+
+        for (i = 1; i <= N; i++) {
+            powerOfTwo = (int) Math.pow(2, i);
+            System.out.println(powerOfTwo + " ");
+    }
+
+	}
+}
