3a7fe500870ba4b583e01edf49b5f693c7471021
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-11-23 12:55:57 +0530

    Add: Program for swaping two numbers entered by user.

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+		int a, b, c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Value in a :");
+        a=sc.nextInt();
+        System.out.println("Enter Value in b :");
+        b=sc.nextInt();
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("Now Values in a is:" +a);
+        System.out.println("Now Values in b is:" +b);
+        sc.close();
+	}
+}
